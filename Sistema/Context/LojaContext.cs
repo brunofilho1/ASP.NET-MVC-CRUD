2b9e002a7e8e7ea8acb2418fdd68d380b3fb84f2
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-06-06 09:54:38 -0300

    Add project files.

---
+++ Sistema/Context/LojaContext.cs
@@ -0,0 +1,14 @@
+﻿using Sistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sistema.Context
+{
+    public class LojaContext : DbContext // passa a ser uma classe que recebe informações do DB
+    {
+        public DbSet<Produto> Produto { get; set; } // aceita valores do tipo model, que tem referência da classe Produto    
+    }
+}
